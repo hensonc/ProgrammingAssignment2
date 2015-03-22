42a01c70d7704a23e7c2bbb22658e416aee16b18
Charlotte Tolonen <charlotte@broadinstitute.org>
2015-03-22 08:07:18 -0400

    Edited template to complete assignment
    Coursera template from rdpeng edited to complete assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix will generate a square, invertable matrix
+## cacheSolve will check for a cached version of the matrix and 
+## generate one if needed.
 
-## Write a short comment describing this function
+
+## Creates a function with its own environment to cache results
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL #initialize the variable for cache
+    set <- function(y) { #set the matrix, y being the variable passed into the function
+      x <<- matrix(rnorm(y), y, y) #assign matrix to variable x
+      m <<- NULL #assign m to be null
+    }
+    get <- function() x #make a function 'get' for makeCacheMatrix
+    setsolve <- function(solve) m <<- solve #takes a value ('solve') and sets it to the value of 'm' in this environment
+    getsolve <- function() m #returns the value of 'm' from this environment
+    list(set = set, get = get, #store these methods in a list so that they can be accessed using $
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+## Checks for and then generates the inverse of the matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()             #Assigns to 'm' the value from x environment
+  message("Checking cache...")
+  if(!is.null(m)) {             #If the 'x' environment exists, the function prints message and the value of m      
+    message("Getting cached data")
+    return(m)
+  }
+  message("Setting new cached value...")
+  data <- x$get()       #If this 'x' has never been evaluted before, pull the x-matrix into 'data'
+  m <- solve(data, ...) #Calculate the inverse of the matrix x by calling 'solve' on 'data'
+  x$setsolve(m)         #Assign the result to the 'x' environment using the 'setsolve' function.
+  m                     #display m
 }
